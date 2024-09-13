c73405d6d059b81f2aece38ea07f3bb29d4aa375
kenta-kenta <kenken22352235@gmail.com>
2024-09-13 10:59:16 +0900

    7-2 独自エラー型への置き換え-サービス層編を学習完了

--- myapi/services/article_service.go
+++ myapi/services/article_service.go
@@ -1,6 +1,10 @@
 package services
 
 import (
+	"database/sql"
+	"errors"
+
+	"github.com/kenta-kenta/go-intermediate-myapi/apperrors"
 	"github.com/kenta-kenta/go-intermediate-myapi/models"
 	"github.com/kenta-kenta/go-intermediate-myapi/repositories"
 )
@@ -10,12 +14,18 @@ func (s *MyAppService) GetArticleService(articleID int) (models.Article, error) 
 	// 1. repositories層の関数SelectArticleDetailで記事の詳細を取得
 	article, err := repositories.SelectArticleDetail(s.db, articleID)
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			err = apperrors.NAData.Wrap(err, "no data")
+			return models.Article{}, err
+		}
+		err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
 		return models.Article{}, err
 	}
 
 	// 2. repositories層の関数SelectCommentListでコメント一覧を取得
 	commentList, err := repositories.SelectCommentList(s.db, articleID)
 	if err != nil {
+		err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
 		return models.Article{}, err
 	}
 
@@ -28,23 +38,35 @@ func (s *MyAppService) GetArticleService(articleID int) (models.Article, error) 
 func (s *MyAppService) PostArticleService(article models.Article) (models.Article, error) {
 	newArticle, err := repositories.InsertArticle(s.db, article)
 	if err != nil {
+		err = apperrors.InsertDataFailed.Wrap(err, "fail to record data")
 		return models.Article{}, err
 	}
 	return newArticle, nil
 }
 
 func (s *MyAppService) GetArticleListService(page int) ([]models.Article, error) {
-	articleArray, err := repositories.SelectArticleList(s.db, page)
+	articleList, err := repositories.SelectArticleList(s.db, page)
 	if err != nil {
+		err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
 		return nil, err
 	}
 
-	return articleArray, nil
+	if len(articleList) == 0 {
+		err := apperrors.NAData.Wrap(ErrNoData, "no data")
+		return nil, err
+	}
+
+	return articleList, nil
 }
 
 func (s *MyAppService) PostNiceService(article models.Article) (models.Article, error) {
 	err := repositories.UpdateNiceNum(s.db, article.ID)
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			err = apperrors.NAData.Wrap(err, "does not exist target article")
+			return models.Article{}, err
+		}
+		err = apperrors.UpdateDataFailed.Wrap(err, "fail to update nice count")
 		return models.Article{}, err
 	}
 
