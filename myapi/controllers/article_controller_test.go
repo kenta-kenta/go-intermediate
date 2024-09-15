853eab51a71db74ea542e786f41cf803a7916681
kenta-kenta <kenken22352235@gmail.com>
2024-09-15 16:44:31 +0900

    8-3 gorilla/muxによるルーティングが絡んだハンドラユニットテストを学習完了

--- myapi/controllers/article_controller_test.go
+++ myapi/controllers/article_controller_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
 func TestArticleListHandler(t *testing.T) {
@@ -27,6 +29,37 @@ func TestArticleListHandler(t *testing.T) {
 
 			// 	ハンドラに引数を渡して実行
 			aCon.ArticleListHandler(res, req)
+
+			// httptest.ResponseRecoder型のCodeフィールドが期待通りかチェック
+			if res.Code != tt.resultCode {
+				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, res.Code)
+			}
+		})
+	}
+}
+
+func TestArticleDetailHandler(t *testing.T) {
+	var tests = []struct {
+		name       string
+		articleID  string
+		resultCode int
+	}{
+		{name: "number pathparam", articleID: "1", resultCode: http.StatusOK},
+		{name: "alphabet pathparam", articleID: "aaa", resultCode: http.StatusNotFound},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// w http.RequestWriter, req *http.Request を用意する
+			url := fmt.Sprintf("http://localhost:8080/article/%s", tt.articleID)
+			req := httptest.NewRequest(http.MethodGet, url, nil)
+
+			res := httptest.NewRecorder()
+
+			// 	ハンドラに引数を渡して実行
+			r := mux.NewRouter()
+			r.HandleFunc("/article/{id:[0-9]+}", aCon.ArticleDetailHandler).Methods(http.MethodGet)
+			r.ServeHTTP(res, req)
 
 			// httptest.ResponseRecoder型のCodeフィールドが期待通りかチェック
 			if res.Code != tt.resultCode {
