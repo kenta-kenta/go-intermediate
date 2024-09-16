a8296c606be46fe20386cc4beb4fe90a29f7a344
kenta-kenta <kenken22352235@gmail.com>
2024-09-16 10:29:14 +0900

    9-3 送った内容をログに出力しようを学習完了

--- myapi/api/middlewares/logging.go
+++ myapi/api/middlewares/logging.go
@@ -5,11 +5,36 @@ import (
 	"net/http"
 )
 
+// 自作ResponseWriterを作る
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+// コンストラクタを作る
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+// WriteHeaderメソッドを作る
+func (rsw *resLoggingWriter) WriteHeader(code int) {
+	rsw.code = code
+	rsw.ResponseWriter.WriteHeader(code)
+}
+
+// ミドルウェアの中身
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		// リクエスト情報をロギング
 		log.Println(req.RequestURI, req.Method)
 
-		next.ServeHTTP(w, req)
+		// 自作のResponseWriterを作って
+		rlw := NewResLoggingWriter(w)
+
+		// それをハンドラに渡す
+		next.ServeHTTP(rlw, req)
+
+		// 自作ResponseWriterからロギングしたいデータを出す
+		log.Println("res: ", rlw.code)
 	})
 }
