13eea2a5dbeb82ac035bc0963c0c77d97fbfedda
kenta-kenta <kenken22352235@gmail.com>
2024-09-27 16:15:07 +0900

    12-3 Google公式のパッケージで楽に検証しよう

---
+++ 12-chapter/myauth/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/auth/credentials/idtoken"
+)
+
+func main() {
+	googleClientID := "[myClientID]"
+	idToken := `[myIDToken]`
+
+	tokenValidator, err := idtoken.NewValidator(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	payload, err := tokenValidator.Validate(context.Background(), idToken, googleClientID)
+	if err != nil {
+		fmt.Println("validate error:", err)
+		return
+	}
+
+	fmt.Println(payload.Claims["name"])
+}
+
+func NewValidator(ctx context.Context, opts ...ClientOption) (*Validator, error)
+
+tokenValidator, err := idtoken.NewValidator(context.Background())
+if err != nil {
+	fmt.Println(err)
+	return
+}
